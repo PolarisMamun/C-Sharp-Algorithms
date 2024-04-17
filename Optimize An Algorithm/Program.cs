ea12b3f583a2138cd3cac8f4d3c03ebc0a69a801
Munsi Abdul Al Mamun <munsiabd.almamun@petronas.com>
2024-04-18 01:45:46 +0800

    Add project files.

---
+++ Optimize An Algorithm/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Optimize_An_Algorithm
+{
+    public class Program
+    {
+        static int FindMaximum(int a, int b,int c)
+        {
+            if (a > b)
+            {
+                if(a > c || a == c)
+                {
+                    return a;
+                }
+            }
+            if(b > c)
+            {
+                if(b > c || b == c)
+                {
+                    return b;
+                }
+            }
+            return c;
+        }
+
+        static int FindMaximum2(int a, int b,int c)
+        {
+            int max = a;
+            if (b > max)
+            {
+                max = b;
+            }
+            if(c > max)
+            {
+                max = c;
+            }
+            return max;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMaximum2(1,2,3));
+            Console.WriteLine(FindMaximum2(8,8,1));
+            Console.WriteLine(FindMaximum2(3,2,3));
+            Console.WriteLine(FindMaximum2(1,1,9));
+            Console.WriteLine(FindMaximum2(1,9,9));
+
+            Console.ReadLine();
+        }
+    }
+}
